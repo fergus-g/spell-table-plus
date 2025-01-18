d3cd138902f658b894ab61405ef1872abcef104f
fergus-g <fergus.gildea@utrack.com>
2025-01-18 22:55:32 +0000

    Added in functionality to click on card and that sorts it.  But encountered bug where the wrong card is being added.  This is probably due to calling fetchCard the incorrect way.  Needs to be stored outside the calls

--- script.js
+++ script.js
@@ -2,14 +2,31 @@ import fetchCardData from "./cards/fetchCardData.js";
 import sortCard from "./cards/sortCard.js";
 
 const search = document.getElementById("search-card");
+const searchCardContainer = document.getElementById("searched-card");
+let searchedCard = {};
 
 search.addEventListener("click", () => {
+  console.log("Im still active");
   showCard();
 });
 
 async function showCard() {
   let returnedCard = await fetchCardData();
-  let { zone, card } = await sortCard(returnedCard);
+  searchedCard = returnedCard;
+  let { card } = await sortCard(returnedCard);
+  // let cardContainer = document.getElementById(`${zone}-images`);
+
+  // Create and assign the image only after a valid card type is determined
+  let cardImg = document.createElement("img");
+  cardImg.className = "card-img";
+  cardImg.setAttribute("id", card.type_line);
+  cardImg.src = card.image_uris.normal;
+  searchCardContainer.appendChild(cardImg);
+}
+
+searchCardContainer.addEventListener("click", async () => {
+  searchCardContainer.innerHTML = "";
+  let { zone, card } = await sortCard(searchedCard);
   let cardContainer = document.getElementById(`${zone}-images`);
 
   // Create and assign the image only after a valid card type is determined
@@ -18,4 +35,4 @@ async function showCard() {
   cardImg.setAttribute("id", card.type_line);
   cardImg.src = card.image_uris.small;
   cardContainer.appendChild(cardImg);
-}
+});
