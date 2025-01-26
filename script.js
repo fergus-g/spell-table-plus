9bc8e6896f2efb75bc6b8a6c35c41ed33afbdffd
fergus-g <fergus.gildea@utrack.com>
2025-01-26 22:34:07 +0000

    Changed some styling and added hover zoom.  Can draw 7 cards now but there is issues with how its stored.

--- script.js
+++ script.js
@@ -7,37 +7,41 @@ let searchedCard = {};
 let playersHand = [];
 
 search.addEventListener("click", () => {
-  if (Object.keys(searchedCard).length === 0) {
-    showCard();
-  }
+  showCard();
 });
 
 async function showCard() {
-  let returnedCard = await fetchCardData();
-
-  while (
-    returnedCard.type_line.includes("Instant") ||
-    returnedCard.type_line.includes("Sorcery")
-  ) {
-    returnedCard = await fetchCardData();
+  while (playersHand.length < 7) {
+    let returnedCard = await fetchCardData();
+    while (
+      returnedCard.type_line.includes("Instant") ||
+      returnedCard.type_line.includes("Sorcery")
+    ) {
+      returnedCard = await fetchCardData();
+    }
+    playersHand.push(returnedCard);
   }
 
-  searchedCard = returnedCard;
-  let cardImg = document.createElement("img");
-  cardImg.className = "card-img";
-  cardImg.setAttribute("id", returnedCard.type_line);
-  cardImg.src = returnedCard.image_uris.normal;
-  searchCardContainer.appendChild(cardImg);
-}
+  // Use forEach to iterate over playersHand
+  playersHand.forEach((card, index) => {
+    let cardImg = document.createElement("img");
+    cardImg.className = "card-img";
+    cardImg.setAttribute("id", card.type_line);
+    cardImg.src = card.image_uris.small;
+    searchCardContainer.appendChild(cardImg);
 
-searchCardContainer.addEventListener("click", async () => {
-  searchCardContainer.innerHTML = "";
-  let { zone, card } = await sortCard(searchedCard);
-  let cardContainer = document.getElementById(`${zone}-images`);
-  let cardImg = document.createElement("img");
-  cardImg.className = "card-img";
-  cardImg.setAttribute("id", card.type_line);
-  cardImg.src = card.image_uris.small;
-  cardContainer.appendChild(cardImg);
-  searchedCard = {};
-});
+    // Add event listener to each card image
+    cardImg.addEventListener("click", async (event) => {
+      event.target.remove();
+      let { zone, card: sortedCard } = await sortCard(playersHand[index]);
+      let cardContainer = document.getElementById(`${zone}-images`);
+      let sortedCardImg = document.createElement("img");
+      sortedCardImg.className = "card-img";
+      sortedCardImg.setAttribute("id", sortedCard.type_line);
+      sortedCardImg.src = sortedCard.image_uris.small;
+      cardContainer.appendChild(sortedCardImg);
+    });
+  });
+
+  searchedCard = playersHand[playersHand.length - 1];
+}
