a37d055a42d83e768a8a9158af2879469dafde36
fergus-g <fergus.gildea@utrack.com>
2025-01-29 17:54:08 +0000

    fixed issue with card clicker not working

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 import fetchCardData from "./cards/fetchCardData.js";
 import sortCard from "./cards/sortCard.js";
 
-const search = document.getElementById("search-card");
+const drawButton = document.getElementById("search-card");
 const searchCardContainer = document.getElementById("searched-card");
 let searchedCard = {};
 let playersHand = [];
 
-search.addEventListener("click", () => {
+drawButton.addEventListener("click", () => {
   showCard();
 });
 
@@ -15,7 +15,8 @@ async function showCard() {
     let returnedCard = await fetchCardData();
     while (
       returnedCard.type_line.includes("Instant") ||
-      returnedCard.type_line.includes("Sorcery")
+      returnedCard.type_line.includes("Sorcery") ||
+      returnedCard.lang !== "en"
     ) {
       returnedCard = await fetchCardData();
     }
@@ -27,20 +28,26 @@ async function showCard() {
     let cardImg = document.createElement("img");
     cardImg.className = "card-img";
     cardImg.setAttribute("id", card.type_line);
-    cardImg.src = card.image_uris.small;
+    cardImg.src = card.image_uris.normal;
     searchCardContainer.appendChild(cardImg);
 
     cardImg.addEventListener("click", async (event) => {
       event.target.remove();
-      let { zone, card: sortedCard } = await sortCard(playersHand[index]);
-      let cardContainer = document.getElementById(`${zone}-images`);
-      let sortedCardImg = document.createElement("img");
-      sortedCardImg.className = "card-img";
-      sortedCardImg.setAttribute("id", sortedCard.type_line);
-      sortedCardImg.src = sortedCard.image_uris.small;
-      cardContainer.appendChild(sortedCardImg);
-      playersHand.splice(index, 1);
-      console.log(playersHand);
+     
+      try {
+        let { zone, card: sortedCard } = await sortCard(card);
+    
+        let cardContainer = document.getElementById(`${zone}-images`);
+        let sortedCardImg = document.createElement("img");
+        sortedCardImg.className = "card-img";
+        sortedCardImg.setAttribute("id", sortedCard.type_line);
+        sortedCardImg.src = sortedCard.image_uris.normal;
+        cardContainer.appendChild(sortedCardImg);
+        playersHand = playersHand.filter((c) => c !== card);
+       
+      } catch (error) {
+        console.error("Error sorting card:", error);
+      }
     });
   });
 
